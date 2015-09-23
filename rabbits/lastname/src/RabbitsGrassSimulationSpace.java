d54b9679d9e7e3deeb4b2fcb60a085264c03f678
Marc Schaer <marc110191@gmail.com>
2015-09-23 10:38:10 +0200

    added TODOs in Space

--- rabbits/lastname/src/RabbitsGrassSimulationSpace.java
+++ rabbits/lastname/src/RabbitsGrassSimulationSpace.java
@@ -8,19 +8,59 @@ import uchicago.src.sim.space.Object2DGrid;
 
 public class RabbitsGrassSimulationSpace {
 
-	private Object2DGrid agentSpace;
+	private Object2DGrid rabbitSpace;
 	private Object2DGrid grassSpace;
 
 	public RabbitsGrassSimulationSpace(int xSize, int ySize) {
-		agentSpace = new Object2DGrid(xSize, ySize);
+		rabbitSpace = new Object2DGrid(xSize, ySize);
 		grassSpace = new Object2DGrid(xSize, ySize);
 
-		// TODO: Put grass in space
+		// TODO: Put 0 grass in space everywhere 
 
 	}
+	
+	public void spreadGrass(int grass) {
+		// spread grass
+	}
+	
+	public int getGrassAt(int x, int y) {
+		
+		return 0;
+	}
 
+	public int getRabbitAt(int x, int y) {
+		
+		return 0;
+	}
+	
+	public Object2DGrid getRabbitSpace() {
+		return rabbitSpace;
+	}
+	
+	public Object2DGrid getGrassSpace() {
+		return grassSpace;
+	}
+	
+	public boolean isCellOccupied(int x, int y) {
+		//TODO
+		return false;
+	}
+	
 	public void addRabbit(RabbitsGrassSimulationAgent rabbit) {
-
+		//TODO
 	}
-
+	
+	public boolean moveRabbitAt(int x, int y, int newX, int newY) {
+		//TODO
+		return false;
+	}
+	
+	public int getTotalGrass() {
+		//TODO
+		return 0;
+	}
+	
+	public void removeRabbitAt(int x, int y) {
+		//TODO
+	}
 }
