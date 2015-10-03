f0a3095656d564aa64bb82d5c7c0fc9ab0ae2299
Marc Schaer <marc110191@gmail.com>
2015-10-03 17:39:06 +0200

    add matrix Tsa

--- reactive/src/template/ReactiveTemplate.java
+++ reactive/src/template/ReactiveTemplate.java
@@ -1,5 +1,6 @@
 package template;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -46,7 +47,29 @@ public class ReactiveTemplate implements ReactiveBehavior {
 			idC1++;
 			idC2 = 0;
 		}
-		printMatrix(r, cities.size(), cities.size());
+		//FIXME : to remove
+		//printMatrix(r, cities.size(), cities.size());
+		
+		// Transition matrix T(s,a)
+		// 				C1	C2	... Cn
+		//noDeliver
+		//DeliverToC1
+		//...
+		//DeliverToCn
+		City[][] Tsa = new City[cities.size()][cities.size()];
+		// First line of the matrix (closer city)
+		for (int i = 0; i < cities.size(); i++) {
+			Tsa[0][i] = closestNeighbour(cities.get(i));
+		}
+		
+		// Rest of the matrix
+		for (int i = 1; i < cities.size(); i++) {
+			for (int j = 0; j < cities.size(); j++) {
+				Tsa[i][j] = cities.get((j+i)%cities.size());
+			}
+		}
+		//FIXME : to remove
+		printTas(Tsa, cities.size(), cities.size());
 	}
 
 	@Override
@@ -65,12 +88,38 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		return action;
 	}
 	
+	private City closestNeighbour(City city) {
+		City closestNeighbour = null;
+		double minDistance = -1;
+		List<City> neighbours = city.neighbors();
+		Iterator<City> it = neighbours.iterator();
+		while (it.hasNext()) {
+			City neighbour = (City) it.next();
+			if (city.distanceTo(neighbour) < minDistance || minDistance < 0) {
+				minDistance = city.distanceTo(neighbour);
+				closestNeighbour = neighbour;
+			}
+		}
+		return closestNeighbour;
+	}
+	
 	private void printMatrix(Number[][] matrix, int sizeX, int sizeY) {
 		int i = 0;
 		int j = 0;
 		for (i = 0; i < sizeX; i++) {
 			for (j = 0; j < sizeY; j++) {
 				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	private void printTas(City[][] cities, int sizeX, int sizeY) {
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < sizeX; i++) {
+			for (j = 0; j < sizeY; j++) {
+				System.out.print(cities[i][j].name + " ");
 			}
 			System.out.println("");
 		}
