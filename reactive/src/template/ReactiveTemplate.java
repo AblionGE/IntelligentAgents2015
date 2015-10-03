f6069805ab08277eabf8237a3b7c8de9f9c54f6b
Marc Schaer <marc110191@gmail.com>
2015-10-03 17:13:39 +0200

    load r and p

--- reactive/src/template/ReactiveTemplate.java
+++ reactive/src/template/ReactiveTemplate.java
@@ -1,5 +1,6 @@
 package template;
 
+import java.util.List;
 import java.util.Random;
 
 import logist.simulation.Vehicle;
@@ -17,17 +18,35 @@ public class ReactiveTemplate implements ReactiveBehavior {
 
 	private Random random;
 	private double pPickup;
+	private Integer[][] p;
+	private Double[][] r;
 
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
 
 		// Reads the discount factor from the agents.xml file.
 		// If the property is not present it defaults to 0.95
-		Double discount = agent.readProperty("discount-factor", Double.class,
-				0.95);
+		Double discount = agent.readProperty("discount-factor", Double.class, 0.95);
 
 		this.random = new Random();
 		this.pPickup = discount;
+
+		// Set matrices r (rewards) and p (probability to have a task from city1 to city2)
+		List<City> cities = topology.cities();
+		int idC1 = 0;
+		int idC2 = 0;
+		r = new Double[cities.size()+1][cities.size()+1];
+		p = new Integer[cities.size()+1][cities.size()+1];
+		for (City c1 : cities) {
+			for (City c2 : cities) {
+				r[idC1][idC2] = td.probability(c1, c2);
+				p[idC1][idC2] = td.reward(c1, c2);
+				idC2++;
+			}
+			idC1++;
+			idC2 = 0;
+		}
+		printMatrix(r, cities.size(), cities.size());
 	}
 
 	@Override
@@ -38,9 +57,22 @@ public class ReactiveTemplate implements ReactiveBehavior {
 			City currentCity = vehicle.getCurrentCity();
 			action = new Move(currentCity.randomNeighbor(random));
 		} else {
-			//Here we need to decide if the task should be taken or not.
+			// Here we need to decide if the task should be taken or not.
 			action = new Pickup(availableTask);
+			System.out.println("pickup : " + availableTask.pickupCity.name);
+			System.out.println("delivery : " + availableTask.deliveryCity.name);
 		}
 		return action;
+	}
+	
+	private void printMatrix(Number[][] matrix, int sizeX, int sizeY) {
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < sizeX; i++) {
+			for (j = 0; j < sizeY; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println("");
+		}
 	}
 }
