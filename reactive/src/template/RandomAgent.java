f386d66f4a8ae437cb6e32a843455d041ff18c20
Marc Schaer <marc110191@gmail.com>
2015-10-14 14:15:38 +0200

    rename randomAgent class

--- reactive/src/template/RandomAgent.java
+++ reactive/src/template/RandomAgent.java
@@ -13,7 +13,7 @@ import logist.task.TaskDistribution;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
 
-public class ReactiveTemplate extends ReactiveAbstractAgent implements ReactiveBehavior {
+public class RandomAgent extends ReactiveAbstractAgent implements ReactiveBehavior {
 
 	private Random random;
 	private double pPickup;
