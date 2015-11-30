25daf684abd2e61422727c8b4a4cb93c15447ee1
Marc Schaer <marc110191@gmail.com>
2015-11-30 10:33:34 +0100

    come back with totalBid*

--- auction/src/template/AuctionIntelligentAgent1.java
+++ auction/src/template/AuctionIntelligentAgent1.java
@@ -122,13 +122,16 @@ public class AuctionIntelligentAgent1 implements AuctionBehavior {
 			double variance = bidVariance[pick][del];
 			bidVariance[pick][del] = (occurence - 1) * variance / occurence
 					+ Math.pow(winBid - expectation, 2) / (occurence + 1);
+
+			totalBidExpectation = (totalBidExpectation * (totalNbOfTasks - 1) + winBid) / totalNbOfTasks;
+			totalBidVariance = (totalNbOfTasks - 2) * totalBidVariance / (totalNbOfTasks - 1)
+					+ Math.pow(winBid - totalBidExpectation, 2) / totalNbOfTasks;
 		} else {
 			bidExpectations[pick][del] = winBid;
 			bidVariance[pick][del] = 0.0;
+			totalBidExpectation = winBid;
+			totalBidVariance = 0.0;
 		}
-		totalBidExpectation = (totalBidExpectation * (totalNbOfTasks - 1) + winBid) / totalNbOfTasks;
-		totalBidVariance = (totalNbOfTasks - 2) * totalBidVariance / (totalNbOfTasks - 1)
-				+ Math.pow(winBid - totalBidExpectation, 2) / totalNbOfTasks;
 		taskOccurences[pick][del] += 1;
 	}
 
